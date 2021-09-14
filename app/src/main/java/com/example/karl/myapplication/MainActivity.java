cf5ba3234f31da04a223dfbb3bb9b59c7c16943c
caleblipan <apesiapesi990320@gmail.com>
2021-09-14 15:17:23 +0700

    Initial commit

--- app/src/main/java/com/example/karl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/karl/myapplication/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.karl.myapplication;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);        
+		getSupportActionBar().setTitle("04a. Image View"); // for set actionbar title
+
 	}
 }
