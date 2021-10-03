efb59f4caaf7d1eb8b3e038a8eaf0b9c960c5d2b
Caleb <apesiapesi990320@gmail.com>
2021-10-03 18:12:52 +0700

    Migrated to AndroidX

--- app/src/main/java/com/example/scheduler/myapplication/MainActivity.java
+++ app/src/main/java/com/example/scheduler/myapplication/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.scheduler.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
--- app/src/main/java/com/example/scheduler/myapplication/RegisterForm.java
+++ app/src/main/java/com/example/scheduler/myapplication/RegisterForm.java
@@ -1,23 +1,8 @@
 package com.example.scheduler.myapplication;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.StyleSpan;
-import android.widget.TextView;
-import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;  
-import android.widget.ArrayAdapter;  
-import android.widget.Spinner;  
-import android.widget.Toast;
 
 public class RegisterForm extends AppCompatActivity {
 	Button button;
