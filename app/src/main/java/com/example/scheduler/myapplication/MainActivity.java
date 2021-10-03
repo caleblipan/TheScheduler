73d6112329af913ef0d40adb009f63fba064b2a8
Caleb <apesiapesi990320@gmail.com>
2021-10-03 22:09:40 +0700

    Added the layout of the homepage

--- app/src/main/java/com/example/scheduler/myapplication/MainActivity.java
+++ app/src/main/java/com/example/scheduler/myapplication/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.scheduler.myapplication;
 
 import android.content.Context;
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 	Button button;
-
+	private BottomNavigationView bottomNavigationView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
