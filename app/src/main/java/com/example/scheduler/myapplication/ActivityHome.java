fe497bf5b05f2f577a41e374197840d811f3bc7f
Caleb <apesiapesi990320@gmail.com>
2021-10-06 17:49:28 +0700

    FIX: Bottom navbar bug

--- app/src/main/java/com/example/scheduler/myapplication/ActivityHome.java
+++ app/src/main/java/com/example/scheduler/myapplication/ActivityHome.java
@@ -3,21 +3,26 @@ package com.example.scheduler.myapplication;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class ActivityHome extends AppCompatActivity {
+public class ActivityHome extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private int loading_time = 5000;
     Button button;
-    private BottomNavigationView bottomNavigationView;
+
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,28 +32,41 @@ public class ActivityHome extends AppCompatActivity {
         actionBar.setTitle("The Scheduler");
         actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar_bg));
 
-        // Move to login form
-        moveToLoginForm();
+        // Bottom navigation menu
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home_button);
     }
 
+    Home home = new Home();
+    List list = new List();
+    Schedule schedule = new Schedule();
+    Login login = new Login();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, home).commit();
+                return true;
+            case R.id.list_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, list).commit();
+                return true;
+            case R.id.schedule_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, schedule).commit();
+                return true;
+            case R.id.login_button:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, login).commit();
+                return true;
+        }
+        return false;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_activity_menu, menu);
         return true;
-    }
-
-    public void moveToLoginForm() {
-        final Context context = this;
-        button = (Button) findViewById(R.id.LoginButton);
-
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent intent = new Intent(context, LoginForm.class);
-                startActivity(intent);
-            }
-        });
     }
 }
