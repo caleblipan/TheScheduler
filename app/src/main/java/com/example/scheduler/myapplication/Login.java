282da084ce830381b1efcc1200d3a087b5b843a0
Caleb <apesiapesi990320@gmail.com>
2021-10-08 12:46:46 +0700

    ADDED: Switching functionality between login & register fragments

--- app/src/main/java/com/example/scheduler/myapplication/Login.java
+++ app/src/main/java/com/example/scheduler/myapplication/Login.java
@@ -1,15 +1,14 @@
 package com.example.scheduler.myapplication;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Login extends Fragment {
-
+    private View rootView;
     public Login() {
         // Required empty public constructor
     }
@@ -18,6 +17,17 @@ public class Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        TextView noAccountButton = (TextView) rootView.findViewById(R.id.noAccount);
+        noAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Register register= new Register();
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, register)
+                        .commit();
+            }
+        });
+        return rootView;
     }
 }
--- app/src/main/java/com/example/scheduler/myapplication/Register.java
+++ app/src/main/java/com/example/scheduler/myapplication/Register.java
@@ -1,21 +1,33 @@
 package com.example.scheduler.myapplication;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class Register extends Fragment {
+import android.widget.TextView;
 
+public class Register extends Fragment {
+    private View rootView;
     public Register() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        rootView = inflater.inflate(R.layout.fragment_register, container, false);
+        TextView haveAnAccountButton = (TextView) rootView.findViewById(R.id.haveAnAccount);
+        haveAnAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Login login= new Login();
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, login)
+                        .commit();
+            }
+        });
+        return rootView;
     }
 }
